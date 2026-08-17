d2bbd6cd265a9ab253ce2cd00b71e7805ff5e0bc
Amrutha-007 <amruthaos154@gmail.com>
2026-08-17 09:32:53 +0530

    Fix production settings

--- GratitudeBackend/settings.py
+++ GratitudeBackend/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-09zmb%@+29x2l85ug0tzfmozw3_$a(bzd-^wc^7ex+nriy4k$$'
+import os
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gratitudejournal-267n.onrender.com']
 
 
 # Application definition
