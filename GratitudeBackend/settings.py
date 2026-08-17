a545fb3e4b6ab197540aa51df849fd707e06a5ca
Amrutha-007 <amruthaos154@gmail.com>
2026-08-17 11:14:51 +0530

    Set DEBUG to False for production

--- GratitudeBackend/settings.py
+++ GratitudeBackend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-local-development-key
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'gratitudejournal-267n.onrender.com']
 
